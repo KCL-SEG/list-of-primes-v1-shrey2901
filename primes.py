cc52a40f97843e21c5649d15bf27c241267f98d8
Shrey <shreyansh290103@outlook.com>
2023-10-12 13:49:34 +0100

    Add a function to return prime numbers
    A different function defined to check if a number is a prime number or not to achieve Modularity and make the code loosely coupled. It also provides a more cohesive design.  Moreover, the check for the factors is done in a sligtly unconverntional way (starting from 0 to num/2)

--- primes.py
+++ primes.py
@@ -1,6 +1,21 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def isPrime(num):
+    count = 0
+    for i in range(2,int(num/2)+1,1):
+        if num % i == 0:
+            count += 1
+    return count
+
+
 def primes(number_of_primes):
     list = []
+    count = 0
+    i = 1
+    while count< number_of_primes:
+        i += 1
+        if isPrime(i) < 1:
+            list.insert(count,i)
+            count += 1
     return list
